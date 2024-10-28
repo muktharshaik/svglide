598efc36b8310cf25a0975613689cffe075a5b01
Burak Sakalli <buraksakalli06@gmail.com>
2024-10-28 16:54:50 +0300

    feat: create icons.ts file to export icons

---
+++ src/icons/icons.ts
@@ -0,0 +1,589 @@
+import { Activity } from "./activity";
+import { Airplay } from "./airplay";
+import { AlertCircle } from "./alert-circle";
+import { AArowDown } from "./a-arrow-down";
+import { AArowUp } from "./a-arrow-up";
+import { ALargeSmall } from "./a-large-small";
+import { Accesibility } from "./accesibility";
+import { AirVent } from "./air-vent";
+import { AlarmClock } from "./alarm-clock";
+import { AlarmClockCheck } from "./alarm-clock-check";
+import { AlarmClockMinus } from "./alarm-clock-minus";
+import { AlarmClockOff } from "./alarm-clock-off";
+import { AlarmClockPlus } from "./alarm-clock-plus";
+import { AlignCenter } from "./align-center";
+import { AlignCenterHorizontal } from "./align-center-horizontal";
+import { AlignCenterVertical } from "./align-center-vertical";
+import { AlignEndHorizontal } from "./align-end-horizontal";
+import { AlignEndVertical } from "./align-end-vertical";
+import { AlignHorizontalDistributeCenter } from "./align-horizontal-distribute-center";
+import { AlignHorizontalDistributeEnd } from "./align-horizontal-distribute-end";
+import { AlignHorizontalDistributeStart } from "./align-horizontal-distribute-start";
+import { AlignHorizontalJustifyCenter } from "./align-horizontal-justify-center";
+import { AlignHorizontalJustifyEnd } from "./align-horizontal-justify-end";
+import { AlignHorizontalJustifyStart } from "./align-horizontal-justify-start";
+import { AlignHorizontalSpaceAround } from "./align-horizontal-space-around";
+import { AlignHorizontalSpaceBetween } from "./align-horizontal-space-between";
+import { AlignJustify } from "./align-justify";
+import { AlignLeft } from "./align-left";
+import { AlignRight } from "./align-right";
+import { AlignStartHorizontal } from "./align-start-horizontal";
+import { AlignStartVertical } from "./align-start-vertical";
+import { AlignVerticalDistributeCenter } from "./align-vertical-distribute-center";
+import { AlignVerticalDistributeEnd } from "./align-vertical-distribute-end";
+import { AlignVerticalDistributeStart } from "./align-vertical-distribute-start";
+import { AlignVerticalSpaceBetween } from "./align-vertical-space-between";
+import { AlignVerticalJustifyCenter } from "./align-vertical-justify-center";
+import { AlignVerticalJustifyEnd } from "./align-vertical-justify-end";
+import { AlignVerticalJustifyStart } from "./align-vertical-justify-start";
+import { AlignVerticalSpaceAround } from "./align-vertical-space-around";
+import { CornerDownLeft } from "./corner-down-left";
+import { CornerDownRight } from "./corner-down-right";
+import { CornerLeftDown } from "./corner-left-down";
+import { CornerLeftUp } from "./corner-left-up";
+import { CornerRightDown } from "./corner-right-down";
+import { CornerRightUp } from "./corner-right-up";
+import { CornerUpLeft } from "./corner-up-left";
+import { CornerUpRight } from "./corner-up-right";
+import { IterationCcw } from "./iteration-ccw";
+import { IterationCw } from "./iteration-cw";
+import { LogIn } from "./log-in";
+import { LogOut } from "./log-out";
+import { MoveDiagonal } from "./move-diagonal";
+import { MoveDiagonal2 } from "./move-diagonal-2";
+import { MoveDown } from "./move-down";
+import { MoveDownLeft } from "./move-down-left";
+import { MoveDownRight } from "./move-down-right";
+import { MoveHorizontal } from "./move-horizontal";
+import { MoveLeft } from "./move-left";
+import { MoveRight } from "./move-right";
+import { MoveUp } from "./move-up";
+import { MoveUpLeft } from "./move-up-left";
+import { MoveUpRight } from "./move-up-right";
+import { MoveVertical } from "./move-vertical";
+import { RotateCcw } from "./rotate-ccw";
+import { RotateCw } from "./rotate-cw";
+import { SkipBack } from "./skip-back";
+import { SkipForward } from "./skip-forward";
+import { StepBack } from "./step-back";
+import { StepForward } from "./step-forward";
+import { UnfoldHorizontal } from "./unfold-horizontal";
+import { UnfoldVertical } from "./unfold-vertical";
+import { ArrowBigDown } from "./arrow-big-down";
+import { ArrowBigDownDash } from "./arrow-big-down-dash";
+import { ArrowBigLeft } from "./arrow-big-left";
+import { ArrowBigLeftDash } from "./arrow-big-left-dash";
+import { ArrowBigRight } from "./arrow-big-right";
+import { ArrowBigRightDash } from "./arrow-big-right-dash";
+import { ArrowBigUp } from "./arrow-big-up";
+import { ArrowBigUpDash } from "./arrow-big-up-dash";
+import { ArrowDown } from "./arrow-down";
+import { ArrowDown01 } from "./arrow-down-0-1";
+import { ArrowDown10 } from "./arrow-down-1-0";
+import { ArrowDownAZ } from "./arrow-down-a-z";
+import { ArrowDownFromLine } from "./arrow-down-from-line";
+import { ArrowDownLeft } from "./arrow-down-left";
+import { ArrowDownNarrowWide } from "./arrow-down-narrow-wide";
+import { ArrowDownWideNarrow } from "./arrow-down-wide-narrow";
+import { ArrowDownRight } from "./arrow-down-right";
+import { ArrowDownToDot } from "./arrow-down-to-dot";
+import { ArrowDownToLine } from "./arrow-down-to-line";
+import { ArrowDownUp } from "./arrow-down-up";
+import { ArrowDownZA } from "./arrow-down-z-a";
+import { ArrowLeft } from "./arrow-left";
+import { ArrowLeftFromLine } from "./arrow-left-from-line";
+import { ArrowLeftRight } from "./arrow-left-right";
+import { ArrowLeftToLine } from "./arrow-left-to-line";
+import { ArrowRight } from "./arrow-right";
+import { ArrowRightFromLine } from "./arrow-right-from-line";
+import { ArrowRightLeft } from "./arrow-right-left";
+import { ArrowRightToLine } from "./arrow-right-to-line";
+import { ArrowUp } from "./arrow-up";
+import { ArrowUp01 } from "./arrow-up-0-1";
+import { ArrowUp10 } from "./arrow-up-1-0";
+import { ArrowUpAZ } from "./arrow-up-a-z";
+import { ArrowUpDown } from "./arrow-up-down";
+import { ArrowUpFromDot } from "./arrow-up-from-dot";
+import { ArrowUpFromLine } from "./arrow-up-from-line";
+import { ArrowUpLeft } from "./arrow-up-left";
+import { ArrowUpNarrowWide } from "./arrow-up-narrow-wide";
+import { ArrowUpRight } from "./arrow-up-right";
+import { ArrowUpToLine } from "./arrow-up-to-line";
+import { ArrowUpWideNarrow } from "./arrow-up-wide-narrow";
+import { ArrowUpZA } from "./arrow-up-z-a";
+import { ArrowsUpFromLine } from "./arrows-up-from-line";
+import { Maximize2 } from "./maximize-2";
+import { Minimize2 } from "./minimize-2";
+import { Move } from "./move";
+import { RefreshCcw } from "./refresh-ccw";
+import { RefreshCcwDot } from "./refresh-ccw-dot";
+import { RefreshCw } from "./refresh-cw";
+import { RefreshCwOff } from "./refresh-cw-off";
+import { Repeat } from "./repeat";
+import { Repeat2 } from "./repeat-2";
+import { TrendingUpDown } from "./trending-up-down";
+import { CircleArrowDown } from "./circle-arrow-down";
+import { CircleArrowLeft } from "./circle-arrow-left";
+import { CircleArrowRight } from "./circle-arrow-right";
+import { CircleArrowUp } from "./circle-arrow-up";
+import { CircleArrowOutDownLeft } from "./circle-arrow-out-down-left";
+import { CircleArrowOutDownRight } from "./circle-arrow-out-down-right";
+import { CircleArrowOutUpLeft } from "./circle-arrow-out-up-left";
+import { CircleArrowOutUpRight } from "./circle-arrow-out-up-right";
+import { SquareArrowDown } from "./square-arrow-down";
+import { SquareArrowDownLeft } from "./square-arrow-down-left";
+import { SquareArrawDownRight } from "./square-arrow-down-right";
+import { SquareArrowLeft } from "./square-arrow-left";
+import { SquareArrowRight } from "./square-arrow-right";
+import { SquareArrowUp } from "./square-arrow-up";
+import { SquareArrowUpLeft } from "./square-arrow-up-left";
+import { SquareArrowUpRight } from "./square-arrow-up-right";
+import { ChartArea } from "./chart-area";
+import { ChartBar } from "./chart-bar";
+import { ChartBarDecreasing } from "./chart-bar-decreasing";
+import { ChartBarIncreasing } from "./chart-bar-increasing";
+import { ChartBarBig } from "./chart-bar-big";
+import { ChartBarStacked } from "./chart-bar-stacked";
+import { ChartCandlestick } from "./chart-candlestick";
+import { ChartColumn } from "./chart-column";
+import { ChartColumnBig } from "./chart-column-big";
+import { ChartColumnDecreasing } from "./chart-column-decreasing";
+import { ChartColumnIncreasing } from "./chart-column-increasing";
+import { ChartColumnStacked } from "./chart-column-stacked";
+import { ChartGantt } from "./chart-gantt";
+import { ChartLine } from "./chart-line";
+import { ChartNetwork } from "./chart-network";
+import { ChartNoAxesColumn } from "./chart-no-axes-column";
+import { ChartNoAxesColumnDecreasing } from "./chart-no-axes-column-decreasing";
+import { ChartNoAxesColumnIncreasing } from "./chart-no-axes-column-increasing";
+import { ChartNoAxesCombined } from "./chart-no-axes-combined";
+import { ChartNoAxesGantt } from "./chart-no-axes-gantt";
+import { ChartPie } from "./chart-pie";
+import { ChartScatter } from "./chart-scatter";
+import { ChartSpline } from "./chart-spline";
+import { Kanban } from "./kanban";
+import { SquareChartGantt } from "./square-chart-gantt";
+import { SquareDashedKanban } from "./square-dashed-kanban";
+import { SquareKanban } from "./square-kanban";
+import { TrendingDown } from "./trending-down";
+import { TrendingUp } from "./trending-up";
+import { FolderKanban } from "./folder-kanban";
+import { PanelsTopLeft } from "./panels-top-left";
+import { Bitcoin } from "./bitcoin";
+import { Chrome } from "./chrome";
+import { Codepen } from "./codepen";
+import { Codesandbox } from "./codesandbox";
+import { Dribbble } from "./dribbble";
+import { Facebook } from "./facebook";
+import { Figma } from "./figma";
+import { Framer } from "./framer";
+import { Github } from "./github";
+import { Gitlab } from "./gitlab";
+import { Hexagon } from "./hexagon";
+import { Instagram } from "./instagram";
+import { Linkedin } from "./linkedin";
+import { Pocket } from "./pocket";
+import { Slack } from "./slack";
+import { Target } from "./target";
+import { Trello } from "./trello";
+import { Twitch } from "./twitch";
+import { Twitter } from "./twitter";
+import { Youtube } from "./youtube";
+import { Baby } from "./baby";
+import { BadgeHelp } from "./badge-help";
+import { BadgeInfo } from "./badge-info";
+import { CircleHelp } from "./circle-help";
+import { Contrast } from "./contrast";
+import { EarOff } from "./ear-off";
+import { Ear } from "./ear";
+import { Eclipse } from "./eclipse";
+import { EyeClosed } from "./eye-closed";
+import { EyeOff } from "./eye-off";
+import { Eye } from "./eye";
+import { Glasses } from "./glasses";
+import { Hand } from "./hand";
+import { Info } from "./info";
+import { LifeBuoy } from "./life-buoy";
+import { MoonStar } from "./moon-star";
+import { Moon } from "./moon";
+import { PersonStanding } from "./person-standing";
+import { ScanEye } from "./scan-eye";
+import { ScanSearch } from "./scan-search";
+import { Speech } from "./speech";
+import { SunDim } from "./sun-dim";
+import { SunMedium } from "./sun-medium";
+import { SunMoon } from "./sun-moon";
+import { Sun } from "./sun";
+import { ZoomIn } from "./zoom-in";
+import { ZoomOut } from "./zoom-out";
+import { Anvil } from "./anvil";
+import { BrickWall } from "./brick-wall";
+import { Building } from "./building";
+import { Building2 } from "./building-2";
+import { Castle } from "./castle";
+import { Church } from "./church";
+import { Cuboid } from "./cuboid";
+import { Dam } from "./dam";
+import { Factory } from "./factory";
+import { Fence } from "./fence";
+import { GraduationCap } from "./graduation-cap";
+import { Hospital } from "./hospital";
+import { Hotel } from "./hotel";
+import { Home } from "./house";
+import { HousePlug } from "./house-plug";
+import { HousePlus } from "./house-plus";
+import { Landmark } from "./landmark";
+import { School } from "./school";
+import { Store } from "./store";
+import { Theater } from "./theater";
+import { University } from "./university";
+import { UtilityPole } from "./utility-pole";
+import { Warehouse } from "./warehouse";
+import { CirclePlus } from "./circle-plus";
+import { Grab } from "./grab";
+import { Lasso } from "./lasso";
+import { LassoSelect } from "./lasso-select";
+import { Loader } from "./loader";
+import { LoaderCircle } from "./loader-circle";
+import { LoaderPinwheel } from "./loader-pinwheel";
+import { MousePointer } from "./mouse-pointer";
+import { MousePointer2 } from "./mouse-pointer-2";
+import { MousePointerBan } from "./mouse-pointer-ban";
+import { MousePointerClick } from "./mouse-pointer-click";
+import { PenTool } from "./pen-tool";
+import { Pencil } from "./pencil";
+import { PencilOff } from "./pencil-off";
+import { Plus } from "./plus";
+import { Pointer } from "./pointer";
+import { PointerOff } from "./pointer-off";
+import { Sparkles } from "./sparkles";
+import { SquareDashedMousePointer } from "./square-dashed-mouse-pointer";
+import { SquareMousePointer } from "./square-mouse-pointer";
+import { Stamp } from "./stamp";
+import { Text } from "./text";
+import { TextCursor } from "./text-cursor";
+import { TextSelect } from "./text-select";
+import { Wand } from "./wand";
+import { WandSparkles } from "./wand-sparkles";
+import { AlarmSmoke } from "./alarm-smoke";
+import { BellElectric } from "./bell-electric";
+import { Blinds } from "./blinds";
+import { Bolt } from "./bolt";
+import { CookingPot } from "./cooking-pot";
+import { DoorClosed } from "./door-closed";
+import { DoorOpen } from "./door-open";
+import { Drill } from "./drill";
+import { Fan } from "./fan";
+import { FireExtinguisher } from "./fire-extinguisher";
+import { Hammer } from "./hammer";
+import { Heater } from "./heater";
+import { Microwave } from "./microwave";
+import { PaintRoller } from "./paint-roller";
+import { Paintbrush } from "./paintbrush";
+import { PaintbrushVertical } from "./paintbrush-vertical";
+import { Refrigerator } from "./refrigerator";
+import { Router } from "./router";
+import { Shell } from "./shell";
+import { ShowerHead } from "./shower-head";
+import { SwatchBook } from "./swatch-book";
+import { Usb } from "./usb";
+import { Vault } from "./vault";
+import { Volleyball } from "./volleyball";
+import { WashingMachine } from "./washing-machine";
+import { Wrench } from "./wrench";
+
+export const Icons = [
+  Activity,
+  Airplay,
+  AlertCircle,
+  AArowDown,
+  AArowUp,
+  ALargeSmall,
+  Accesibility,
+  AirVent,
+  AlarmClock,
+  AlarmClockCheck,
+  AlarmClockMinus,
+  AlarmClockOff,
+  AlarmClockPlus,
+  AlignCenter,
+  AlignCenterHorizontal,
+  AlignCenterVertical,
+  AlignEndHorizontal,
+  AlignEndVertical,
+  AlignHorizontalDistributeCenter,
+  AlignHorizontalDistributeEnd,
+  AlignHorizontalDistributeStart,
+  AlignHorizontalJustifyCenter,
+  AlignHorizontalJustifyEnd,
+  AlignHorizontalJustifyStart,
+  AlignHorizontalSpaceAround,
+  AlignHorizontalSpaceBetween,
+  AlignJustify,
+  AlignLeft,
+  AlignRight,
+  AlignStartHorizontal,
+  AlignStartVertical,
+  AlignVerticalDistributeCenter,
+  AlignVerticalDistributeEnd,
+  AlignVerticalDistributeStart,
+  AlignVerticalSpaceBetween,
+  AlignVerticalJustifyCenter,
+  AlignVerticalJustifyEnd,
+  AlignVerticalJustifyStart,
+  AlignVerticalSpaceAround,
+  CornerDownLeft,
+  CornerDownRight,
+  CornerLeftDown,
+  CornerLeftUp,
+  CornerRightDown,
+  CornerRightUp,
+  CornerUpLeft,
+  CornerUpRight,
+  IterationCcw,
+  IterationCw,
+  LogIn,
+  LogOut,
+  MoveDiagonal,
+  MoveDiagonal2,
+  MoveDown,
+  MoveDownLeft,
+  MoveDownRight,
+  MoveHorizontal,
+  MoveLeft,
+  MoveRight,
+  MoveUp,
+  MoveUpLeft,
+  MoveUpRight,
+  MoveVertical,
+  RotateCcw,
+  RotateCw,
+  SkipBack,
+  SkipForward,
+  StepBack,
+  StepForward,
+  UnfoldHorizontal,
+  UnfoldVertical,
+  ArrowBigDown,
+  ArrowBigDownDash,
+  ArrowBigLeft,
+  ArrowBigLeftDash,
+  ArrowBigRight,
+  ArrowBigRightDash,
+  ArrowBigUp,
+  ArrowBigUpDash,
+  ArrowDown,
+  ArrowDown01,
+  ArrowDown10,
+  ArrowDownAZ,
+  ArrowDownFromLine,
+  ArrowDownLeft,
+  ArrowDownNarrowWide,
+  ArrowDownWideNarrow,
+  ArrowDownRight,
+  ArrowDownToDot,
+  ArrowDownToLine,
+  ArrowDownUp,
+  ArrowDownZA,
+  ArrowLeft,
+  ArrowLeftFromLine,
+  ArrowLeftRight,
+  ArrowLeftToLine,
+  ArrowRight,
+  ArrowRightFromLine,
+  ArrowRightLeft,
+  ArrowRightToLine,
+  ArrowUp,
+  ArrowUp01,
+  ArrowUp10,
+  ArrowUpAZ,
+  ArrowUpDown,
+  ArrowUpFromDot,
+  ArrowUpFromLine,
+  ArrowUpLeft,
+  ArrowUpNarrowWide,
+  ArrowUpRight,
+  ArrowUpToLine,
+  ArrowUpWideNarrow,
+  ArrowUpZA,
+  ArrowsUpFromLine,
+  Maximize2,
+  Minimize2,
+  Move,
+  RefreshCcw,
+  RefreshCcwDot,
+  RefreshCw,
+  RefreshCwOff,
+  Repeat,
+  Repeat2,
+  TrendingUpDown,
+  CircleArrowDown,
+  CircleArrowLeft,
+  CircleArrowRight,
+  CircleArrowUp,
+  CircleArrowOutDownLeft,
+  CircleArrowOutDownRight,
+  CircleArrowOutUpLeft,
+  CircleArrowOutUpRight,
+  SquareArrowDown,
+  SquareArrowDownLeft,
+  SquareArrawDownRight,
+  SquareArrowLeft,
+  SquareArrowRight,
+  SquareArrowUp,
+  SquareArrowUpLeft,
+  SquareArrowUpRight,
+  ChartArea,
+  ChartBar,
+  ChartBarDecreasing,
+  ChartBarIncreasing,
+  ChartBarBig,
+  ChartBarStacked,
+  ChartCandlestick,
+  ChartColumn,
+  ChartColumnBig,
+  ChartColumnDecreasing,
+  ChartColumnIncreasing,
+  ChartColumnStacked,
+  ChartGantt,
+  ChartLine,
+  ChartNetwork,
+  ChartNoAxesColumn,
+  ChartNoAxesColumnDecreasing,
+  ChartNoAxesColumnIncreasing,
+  ChartNoAxesCombined,
+  ChartNoAxesGantt,
+  ChartPie,
+  ChartScatter,
+  ChartSpline,
+  Kanban,
+  SquareChartGantt,
+  SquareDashedKanban,
+  SquareKanban,
+  TrendingDown,
+  TrendingUp,
+  FolderKanban,
+  PanelsTopLeft,
+  Bitcoin,
+  Chrome,
+  Codepen,
+  Codesandbox,
+  Dribbble,
+  Facebook,
+  Figma,
+  Framer,
+  Github,
+  Gitlab,
+  Hexagon,
+  Instagram,
+  Linkedin,
+  Pocket,
+  Slack,
+  Target,
+  Trello,
+  Twitch,
+  Twitter,
+  Youtube,
+  Baby,
+  BadgeHelp,
+  BadgeInfo,
+  CircleHelp,
+  Contrast,
+  EarOff,
+  Ear,
+  Eclipse,
+  EyeClosed,
+  EyeOff,
+  Eye,
+  Glasses,
+  Hand,
+  Info,
+  LifeBuoy,
+  MoonStar,
+  Moon,
+  PersonStanding,
+  ScanEye,
+  ScanSearch,
+  Speech,
+  SunDim,
+  SunMedium,
+  SunMoon,
+  Sun,
+  ZoomIn,
+  ZoomOut,
+  Anvil,
+  BrickWall,
+  Building,
+  Building2,
+  Castle,
+  Church,
+  Cuboid,
+  Dam,
+  Factory,
+  Fence,
+  GraduationCap,
+  Hospital,
+  Hotel,
+  Home,
+  HousePlug,
+  HousePlus,
+  Landmark,
+  School,
+  Store,
+  Theater,
+  University,
+  UtilityPole,
+  Warehouse,
+  CirclePlus,
+  Grab,
+  Lasso,
+  LassoSelect,
+  Loader,
+  LoaderCircle,
+  LoaderPinwheel,
+  MousePointer,
+  MousePointer2,
+  MousePointerBan,
+  MousePointerClick,
+  PenTool,
+  Pencil,
+  PencilOff,
+  Plus,
+  Pointer,
+  PointerOff,
+  Sparkles,
+  SquareDashedMousePointer,
+  SquareMousePointer,
+  Stamp,
+  Text,
+  TextCursor,
+  TextSelect,
+  Wand,
+  WandSparkles,
+  AlarmSmoke,
+  BellElectric,
+  Blinds,
+  Bolt,
+  CookingPot,
+  DoorClosed,
+  DoorOpen,
+  Drill,
+  Fan,
+  FireExtinguisher,
+  Hammer,
+  Heater,
+  Microwave,
+  PaintRoller,
+  Paintbrush,
+  PaintbrushVertical,
+  Refrigerator,
+  Router,
+  Shell,
+  ShowerHead,
+  SwatchBook,
+  Usb,
+  Vault,
+  Volleyball,
+  WashingMachine,
+  Wrench,
+];
