817054048abb8f3ef355219e5851c8d82399c641
Burak Sakalli <buraksakalli06@gmail.com>
2024-11-17 15:53:29 +0400

    feat: playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { AlarmClock } from "./lib";
+
 export const App = () => {
-  return <div>App</div>;
+  const [hovered, setHovered] = useState<boolean>(false);
+  return (
+    <main
+      style={{
+        margin: "0 auto",
+        maxWidth: "600px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>Svglide Playground</h1>
+
+      <div
+        onPointerEnter={() => setHovered(true)}
+        onPointerLeave={() => setHovered(false)}
+      >
+        <AlarmClock data-hovered={hovered} />
+      </div>
+    </main>
+  );
 };
